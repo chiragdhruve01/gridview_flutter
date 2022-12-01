5acd4bd80f65cddb0b9d57cf3e2459693aa4660a
chiragdhruve01 <chirag@houstonsupport.com>
2022-12-01 15:46:58 +0530

    gridview with play button

--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -31,42 +31,56 @@ class _WelcomePageState extends State<WelcomePage> {
         body: GridView.count(
           crossAxisCount: 3,
           crossAxisSpacing: 10.0,
-          mainAxisSpacing: 10.0,
+          // mainAxisSpacing: 10.0,
           shrinkWrap: true,
-          children: List.generate(
-            5,
-            (index) {
-              return Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: NetworkImage(
-                          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbKV-D7QVkH13KmdFlp81HHhoKWRFcPOo_emwaDJMU&s'),
-                      fit: BoxFit.cover,
-                    ),
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(20.0),
-                    ),
-                  ),
-                  child: Align(
-                    alignment: Alignment.bottomRight,
-                    child: Container(
-                      margin: const EdgeInsets.all(3),
-                      padding: const EdgeInsets.all(3),
-                      decoration: BoxDecoration(
-                          color: Theme.of(context).primaryColor,
-                          borderRadius: BorderRadius.circular(100)),
-                      child: const Icon(
-                        Icons.play_arrow,
-                        color: Colors.white,
+          children: List.generate(5, (index) {
+            return Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: GestureDetector(
+                onTap: () => {print("clicked icons")},
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Expanded(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: NetworkImage(
+                                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbKV-D7QVkH13KmdFlp81HHhoKWRFcPOo_emwaDJMU&s'),
+                              fit: BoxFit.cover,
+                            ),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(20.0),
+                            ),
+                          ),
+                          child: Align(
+                            alignment: Alignment.bottomRight,
+                            child: Container(
+                              margin: const EdgeInsets.all(5),
+                              //padding: const EdgeInsets.all(5),
+                              decoration: BoxDecoration(
+                                  color: Theme.of(context).primaryColor,
+                                  borderRadius: BorderRadius.circular(100)),
+                              child: const Icon(
+                                Icons.play_arrow,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                ),
-              );
-            },
-          ),
+                      const Center(
+                          child: Text(
+                        'hello',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ))
+                    ]),
+              ),
+            );
+          }),
         ),
       ),
     );
