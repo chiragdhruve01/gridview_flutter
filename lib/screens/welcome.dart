00873d3986f339b4843d8e5f35c298dd55281434
chiragdhruve01 <chirag@houstonsupport.com>
2022-12-05 11:45:10 +0530

    welcome screen dynamic updated

--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:item/screens/grid_color.dart';
 import 'package:flutter/foundation.dart' show TargetPlatform;
 import 'package:item/screens/sublist.dart';
+import 'package:item/service/auth_service.dart';
 
 class WelcomePage extends StatefulWidget {
   const WelcomePage({super.key});
@@ -17,53 +18,67 @@ class PhotoItem {
 }
 
 class _WelcomePageState extends State<WelcomePage> {
-  final List<PhotoItem> _items = [
-    PhotoItem(
-        "https://images.pexels.com/photos/1772973/pexels-photo-1772973.png?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "Stephan Seeber"),
-    PhotoItem(
-        "https://images.pexels.com/photos/1758531/pexels-photo-1758531.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "Liam Gant"),
-    PhotoItem(
-        "https://images.pexels.com/photos/1130847/pexels-photo-1130847.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "Stephan Seeber"),
-    PhotoItem(
-        "https://images.pexels.com/photos/45900/landscape-scotland-isle-of-skye-old-man-of-storr-45900.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "Pixabay"),
-    PhotoItem(
-        "https://images.pexels.com/photos/165779/pexels-photo-165779.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "Scott Webb"),
-    PhotoItem(
-        "https://images.pexels.com/photos/548264/pexels-photo-548264.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "Krivec Ales"),
-    PhotoItem(
-        "https://images.pexels.com/photos/188973/matterhorn-alpine-zermatt-mountains-188973.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "Pixabay"),
-    PhotoItem(
-        "https://images.pexels.com/photos/795188/pexels-photo-795188.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "Melanie Wupper"),
-    PhotoItem(
-        "https://images.pexels.com/photos/5222/snow-mountains-forest-winter.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "Jaymantri"),
-    PhotoItem(
-        "https://images.pexels.com/photos/789381/pexels-photo-789381.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "Riciardus"),
-    PhotoItem(
-        "https://images.pexels.com/photos/326119/pexels-photo-326119.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "Pixabay"),
-    PhotoItem(
-        "https://images.pexels.com/photos/707344/pexels-photo-707344.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "Eberhard"),
-    PhotoItem(
-        "https://images.pexels.com/photos/691034/pexels-photo-691034.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "Mirsad Mujanovic"),
-    PhotoItem(
-        "https://images.pexels.com/photos/655676/pexels-photo-655676.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "Vittorio Staffolani"),
-    PhotoItem(
-        "https://images.pexels.com/photos/592941/pexels-photo-592941.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        "Tobi"),
-  ];
+  final AuthService authService = AuthService();
+
+  // final List<PhotoItem> _items = [
+  //   PhotoItem(
+  //       "https://images.pexels.com/photos/1772973/pexels-photo-1772973.png?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  //       "Stephan Seeber"),
+  //   PhotoItem(
+  //       "https://images.pexels.com/photos/1758531/pexels-photo-1758531.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  //       "Liam Gant"),
+  //   PhotoItem(
+  //       "https://images.pexels.com/photos/1130847/pexels-photo-1130847.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  //       "Stephan Seeber"),
+  //   PhotoItem(
+  //       "https://images.pexels.com/photos/45900/landscape-scotland-isle-of-skye-old-man-of-storr-45900.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  //       "Pixabay"),
+  //   PhotoItem(
+  //       "https://images.pexels.com/photos/165779/pexels-photo-165779.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  //       "Scott Webb"),
+  //   PhotoItem(
+  //       "https://images.pexels.com/photos/548264/pexels-photo-548264.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  //       "Krivec Ales"),
+  //   PhotoItem(
+  //       "https://images.pexels.com/photos/188973/matterhorn-alpine-zermatt-mountains-188973.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  //       "Pixabay"),
+  //   PhotoItem(
+  //       "https://images.pexels.com/photos/795188/pexels-photo-795188.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  //       "Melanie Wupper"),
+  //   PhotoItem(
+  //       "https://images.pexels.com/photos/5222/snow-mountains-forest-winter.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  //       "Jaymantri"),
+  //   PhotoItem(
+  //       "https://images.pexels.com/photos/789381/pexels-photo-789381.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  //       "Riciardus"),
+  //   PhotoItem(
+  //       "https://images.pexels.com/photos/326119/pexels-photo-326119.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  //       "Pixabay"),
+  //   PhotoItem(
+  //       "https://images.pexels.com/photos/707344/pexels-photo-707344.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  //       "Eberhard"),
+  //   PhotoItem(
+  //       "https://images.pexels.com/photos/691034/pexels-photo-691034.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  //       "Mirsad Mujanovic"),
+  //   PhotoItem(
+  //       "https://images.pexels.com/photos/655676/pexels-photo-655676.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  //       "Vittorio Staffolani"),
+  //   PhotoItem(
+  //       "https://images.pexels.com/photos/592941/pexels-photo-592941.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  //       "Tobi"),
+  // ];
+  List<dynamic> _items = [];
+  @override
+  void initState() {
+    super.initState();
+    getPrefs();
+  }
+
+  Future<void> getPrefs() async {
+    _items = await authService.getTravelList();
+    setState(() => {_items});
+  }
+
   // This widget is the root of your application
   @override
   Widget build(BuildContext context) {
@@ -133,7 +148,7 @@ class _WelcomePageState extends State<WelcomePage> {
                       await showDialog(
                         context: context,
                         builder: (_) => imageDialog(
-                            'My Image', _items[index].image, context),
+                            'My Image', _items[index]['description'], context),
                       );
                     },
                     child: Column(
@@ -143,7 +158,8 @@ class _WelcomePageState extends State<WelcomePage> {
                             child: Container(
                               decoration: BoxDecoration(
                                 image: DecorationImage(
-                                  image: NetworkImage(_items[index].image),
+                                  image: NetworkImage(
+                                      _items[index]['description']),
                                   fit: BoxFit.cover,
                                 ),
                                 borderRadius: BorderRadius.all(
@@ -168,8 +184,11 @@ class _WelcomePageState extends State<WelcomePage> {
                                             context,
                                             MaterialPageRoute(
                                                 builder: (context) =>
-                                                    SubListPage('Categories',
-                                                        _items[index].name)));
+                                                    SubListPage(
+                                                        'Categories',
+                                                        _items[index]
+                                                                ['place'] ??
+                                                            "")));
                                         print("yeeehhh clicked");
                                       },
                                       icon: Icon(
@@ -185,7 +204,7 @@ class _WelcomePageState extends State<WelcomePage> {
                           ),
                           Center(
                               child: Text(
-                            _items[index].name,
+                            _items[index]['place'] ?? "",
                             style: TextStyle(
                               fontWeight: FontWeight.bold,
                               fontSize: 16,
